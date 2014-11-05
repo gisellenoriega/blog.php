851226aca46268f6b445babd8426c6ad2f5523ae
Giselle Noriega <gnoriega626@gmail.com>
2014-11-05 12:12:07 -0800

    blog
    added and fixed comments

--- view/form.php
+++ view/form.php
@@ -1,18 +1,18 @@
-<h1>Create Blog Post</h1>
+<h1>Create Blog Post</h1> -->
 
 <form>
 	<div>
-	//*this is for title
-	//*label=text thats displayed in front of the text box
-	//*label is for title
+	<!-- this is for title -->
+	<!-- label=text thats displayed in front of the text box -->
+	<!-- label is for title -->
 		<label for="title">Title: </label>
 		<input type="text" name="title" />
 	</div>
-	//* holds our info for our posts
+	<!-- holds our info for our posts -->
 	<div>
-	//*for our posts
-	//*textarea allows us to type as many sentences as we can
-	//*spans up more than one line
+	<!-- for our posts -->
+	<!-- textarea allows us to type as many sentences as we can -->
+	<!-- spans up more than one line -->
 		<label for="post">Post: </label>
 		<textarea name="post"></textarea>
 	</div>
