5ba1b85eaca60d0f555142afee6840728ef7df56
Giselle Noriega <gnoriega626@gmail.com>
2014-11-05 11:53:12 -0800

    blog
    added form.php added 3 require_once's in post.php

--- view/navigation.php
+++ view/navigation.php
@@ -1,7 +1,6 @@
 <?php
 	require_once(__DIR__ . "/../model/config.php");
 ?>
-//*echoes out path and pulls the blog from the model folder then the config file.
 <nav>
 	<ul>
 		<li><a href="<?php echo $path . "post.php"?>">Blog Post Form</a></li>
