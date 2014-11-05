74ae12474b49e10dcee9184fcfd9645ef0aa36c8
Giselle Noriega <gnoriega626@gmail.com>
2014-11-05 12:16:53 -0800

    blog
    added css folder and linked to blog.php

--- blog.php
+++ blog.php
@@ -1,7 +1,15 @@
+<DOCTYPE <!DOCTYPE html>
+<html>
+<link rel="stylesheet" type="text/css" href="main.css">
+<head>
+	<title></title>
+</head>
+<body>
 <?php
     require_once(__DIR__ . "/view/header.php");
     require_once(__DIR__ . "/view/navigation.php");
 	require_once(__DIR__ . "/controller/create-db.php");
 	require_once(__DIR__ . "/view/footer.php");
-
 ?>
+</body>
+</html>
