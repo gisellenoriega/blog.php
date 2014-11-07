cb7d30625bc242563f1f7376ee7bbcb3dc7eff3d
Giselle Noriega <gnoriega626@gmail.com>
2014-11-07 11:06:03 -0800

    blog
    added a file called create-post.php

--- view/form.php
+++ view/form.php
@@ -1,22 +1,20 @@
-<h1>Create Blog Post</h1> -->
-
-<form>
-	<div>
+<h1>Create Blog Post</h1>
 	<!-- this is for title -->
 	<!-- label=text thats displayed in front of the text box -->
 	<!-- label is for title -->
+<form>
+	<div>
 		<label for="title">Title: </label>
 		<input type="text" name="title" />
 	</div>
 	<!-- holds our info for our posts -->
 	<div>
+	<!-- spans up more than one line -->
 	<!-- for our posts -->
 	<!-- textarea allows us to type as many sentences as we can -->
-	<!-- spans up more than one line -->
-		<label for="post">Post: </label>
+			<label for="post">Post: </label>
 		<textarea name="post"></textarea>
 		<!-- submit button submits it somewhere else -->
 		<button type="submit">Submit</button>
 	</div>
 </form>
-?>
