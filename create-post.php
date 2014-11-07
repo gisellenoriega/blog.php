b1a71ef53fd4f5d1e8e176f3be54682d2bb4d9e9
Giselle Noriega <gnoriega626@gmail.com>
2014-11-07 11:11:13 -0800

    blog
    added comments

--- create-post.php
+++ create-post.php
@@ -1,5 +1,6 @@
 <?php
-
+//* two variables that submit data
+// filter validates sanitizes to make sure there are no mailicious things.
 	$title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
 	$post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
 
