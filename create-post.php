9051df1e21ede1c4c3e18854cdc2e831d94c5300
Giselle Noriega <gnoriega626@gmail.com>
2014-11-10 14:35:16 -0800

    blog
    added media queries

--- create-post.php
+++ create-post.php
@@ -1,8 +1,8 @@
 <?php
 //* two variables that submit data
-// filter validates sanitizes to make sure there are no mailicious things.
+//* filter validates sanitizes to make sure there are no mailicious things.
 	$title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
 	$post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
-
+// echoes out the title and post
 	echo "<p>Title: $title</p>";
 	echo "<p>Post: $post</p>"
