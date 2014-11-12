c908011d01a04d0fd767a1fb3d674c1781bbc2ff
Giselle Noriega <gnoriega626@gmail.com>
2014-11-12 12:26:35 -0800

    blog, video 23
    added a directory and $connection and commented

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,8 +1,16 @@
 <?php
 //* two variables that submit data
 //* filter validates sanitizes to make sure there are no mailicious things.
+// DIR means directory and "/../" means finding a folder
+// $host $username $password $database are parameters
+	require_once(__DIR__ . "/../model/database.php");
+
+	$connection = new mysqil($host, $username, $password, $database)
+
 	$title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
 	$post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
 // echoes out the title and post
 	echo "<p>Title: $title</p>";
 	echo "<p>Post: $post</p>";
+
+	$connection->close();
