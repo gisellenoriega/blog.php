b402349de11885b59ce463ade043caf51f9dc630
Giselle Noriega <gnoriega626@gmail.com>
2014-11-13 12:06:59 -0800

    blog, video 24
    added query, if statement, else statement, echoes inside the statements, and commented

--- controller/create-post.php
+++ controller/create-post.php
@@ -5,10 +5,21 @@
 // $host $username $password $database are parameters
 	require_once(__DIR__ . "/../model/database.php");
 
-	$connection = new mysqil($host, $username, $password, $database)
+	$connection = new mysqli($host, $username, $password, $database);
 
 	$title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
 	$post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
+
+// 
+	$query = $connection->query("INSERT INTO posts SET title = '$title', post = '$post'");
+
+	if($query) {
+		echo "<p>Successfully inserted post: $title</p>";
+
+	}
+	else {
+		echo "<p>$connection->error</p>";
+	}
 // echoes out the title and post
 	echo "<p>Title: $title</p>";
 	echo "<p>Post: $post</p>";
