5abe79e28409b829ff5eaa7afc1298b0c15bcf14
Giselle Noriega <gnoriega626@gmail.com>
2014-11-16 10:07:54 -0800

    blog, video 26, creating a database class
    created a class with 4 functions

--- model/config.php
+++ model/config.php
@@ -1,2 +1,7 @@
 <?php
 	$path = "/blog/";
+	
+	$host = "localhost";
+	$username = "root";
+	$password = "root";
+	$database = "blog_db";
--- model/database.php
+++ model/database.php
@@ -1,8 +1,9 @@
 <?php
-
-$host = "localhost";
-$username = "root";
-$password = "root";
-$database = "blog_db";
-
-?>
+class Database {
+	private $connection;
+	private $host;
+	private $username;
+	private $password;
+	private $database;
+// use a class instead of many functions because the code would look neater and to make new objects.
+}
