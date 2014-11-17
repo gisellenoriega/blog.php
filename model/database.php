ac9c0e80918d7284fcad276c914c5a4bd5e41819
Giselle Noriega <gnoriega626@gmail.com>
2014-11-17 13:23:34 -0800

    blog, video 27, Creating a Constructor for Our Database Class
    added public function __construct and 4 $this inside

--- model/database.php
+++ model/database.php
@@ -5,5 +5,15 @@ class Database {
 	private $username;
 	private $password;
 	private $database;
+
+	public function __construct($host, $username , $password, $database){
+		$this->host = $host;
+		$this->username = $username;
+		$this->password = $password;
+		$this->database = $database;
+
+	}
+
 // use a class instead of many functions because the code would look neater and to make new objects.
+// function __construct automatically calls on class instantation
 }
